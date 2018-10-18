63009a0996b12ba1e862f92fc88211d49c62c6c0
Your Name <524146970@qq.com>
2018-10-18 10:35:14 +0800

    second

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: '111222333Welcome to Your Vue.js App333222111'
+      msg: '111222333Welcome to Your Vue.js App'
     }
   }
 }
